85dbe58f20300e18cbe1ddb5f1d2dfd75fb9670a
Felix Kratz <felix.kratz@tu-dortmund.de>
2021-09-06 15:24:44 +0200

    example space script with freezing

--- plugins/space.sh
+++ plugins/space.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 if [ "$SELECTED" = "true" ]; then
+  sketchybar -m freeze on
   sketchybar -m set $NAME icon_highlight on
   sketchybar -m set $NAME label_highlight on
+  sketchybar -m freeze off
 else
+  sketchybar -m freeze on
   sketchybar -m set $NAME icon_highlight off
   sketchybar -m set $NAME label_highlight off
+  sketchybar -m freeze off
 fi
