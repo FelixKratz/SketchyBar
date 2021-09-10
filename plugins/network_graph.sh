8c037b2dfe1b3159eb9e75f0e237ab0285f1daa7
FelixKratz <felix.kratz@tu-dortmund.de>
2021-09-10 12:20:08 +0200

    small changes to the default rc

--- plugins/network_graph.sh
+++ plugins/network_graph.sh
@@ -7,17 +7,3 @@ UP=$(echo $UPDOWN | awk "{ print \$2 }" | cut -f1 -d ".")
 
 sketchybar -m push network_down $(echo $UPDOWN | awk "{ print \$1 / (1.0 * $MAX_DOWN) }")
 sketchybar -m push network_up $(echo $UPDOWN | awk "{ print \$2 / (1.0 * $MAX_UP) }")
-
-DOWN_STR=""
-UP_STR=""
-if [ "$DOWN" -lt "1000" ]; then
-  DOWN_STR="$DOWN kbps"
-else
-  DOWN_STR=$(echo "scale=2; $DOWN" | bc -l)" Mbps"
-fi
-if [ "$UP" -lt "1000" ]; then
-  UP_STR="$UP kbps"
-else
-  UP_STR=$(echo "scale=2; $UP" | bc -l)" Mbps"
-fi
-sketchybar -m set network_label label " $DOWN_STR 祝 $UP_STR"
