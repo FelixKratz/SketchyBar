3c1c7214ee5d8860a95e706f84f329cbcb33a375
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-01-02 14:26:51 +0100

    slim down demo sketchybarrc

--- plugins/network_graph.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-MAX_DOWN=110000
-MAX_UP=45000
-UPDOWN=$(ifstat -i "en0" -b 0.1 1 | tail -n1)
-DOWN=$(echo $UPDOWN | awk "{ print \$1 }" | cut -f1 -d ".")
-UP=$(echo $UPDOWN | awk "{ print \$2 }" | cut -f1 -d ".")
-
-sketchybar -m --push network_down $(echo $UPDOWN | awk "{ print \$1 / (1.0 * $MAX_DOWN) }")
-              --push network_up $(echo $UPDOWN | awk "{ print \$2 / (1.0 * $MAX_UP) }")
--- plugins/space.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$SELECTED" = "true" ]; then
-  sketchybar -m --set $NAME label.highlight=on icon.highlight=on
-else
-  sketchybar -m --set $NAME label.highlight=off icon.highlight=off
-fi
