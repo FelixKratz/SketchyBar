d33450bbe5f309ee89298ebb72199f514701058b
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-05-12 15:21:03 +0200

    documentation initialization

--- plugins/cpu_graph.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
-CPU_INFO=$(ps -eo pcpu,user)
-CPU_SYS=$(echo "$CPU_INFO" | grep -v $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
-CPU_USER=$(echo "$CPU_INFO" | grep $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
-
-sketchybar --push cpu_sys $CPU_SYS \
-           --push cpu_user $CPU_USER
