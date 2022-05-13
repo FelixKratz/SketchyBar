4f259a1b82a99226e9a975c58bc0a7e81de92784
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-05-13 09:08:47 +0200

    small adjustments

---
+++ assets/js/abdf6211.26ee146d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksketchybar_site=self.webpackChunksketchybar_site||[]).push([[643],{3769:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
