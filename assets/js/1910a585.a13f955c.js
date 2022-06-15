6aad7838f7c4f68cae89eb831c8926a11b43f8eb
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-06-15 22:54:43 +0200

    fix up docs for 2.7.0

--- assets/js/1910a585.a13f955c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksketchybar_site=self.webpackChunksketchybar_site||[]).push([[568],{3769:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
