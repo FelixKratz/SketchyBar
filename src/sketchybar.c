1a3708c5addf9616c8eb9833086087c37d95ad3f
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-09-30 13:59:11 +0200

    hotfix for mouse problems and select distributed notifications

--- src/sketchybar.c
+++ src/sketchybar.c
@@ -26,7 +26,7 @@
 
 #define MAJOR 2
 #define MINOR 16
-#define PATCH 3
+#define PATCH 4
 
 extern CGError SLSRegisterNotifyProc(void* callback, uint32_t event, void* context);
 extern int SLSMainConnectionID(void);
