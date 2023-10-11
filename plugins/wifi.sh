f36eb7d672cd6f0ba8b6e951227a9242b8e36b25
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-10-11 19:15:18 +0200

    update default config

--- plugins/wifi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# The wifi_change event supplies a $INFO variable in which the current SSID
-# is passed to the script.
-
-if [ "$SENDER" = "wifi_change" ]; then
-  WIFI=${INFO:-"Not Connected"}
-  sketchybar --set $NAME label="${WIFI}"
-fi
