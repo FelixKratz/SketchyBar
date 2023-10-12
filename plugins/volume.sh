934c0895952c3b2e7149fd04f59411f0ac6f268d
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-10-12 22:06:59 +0300

    Update volume icons

--- plugins/volume.sh
+++ plugins/volume.sh
@@ -7,13 +7,13 @@ if [ "$SENDER" = "volume_change" ]; then
   VOLUME=$INFO
 
   case $VOLUME in
-    [6-9][0-9]|100) ICON="墳"
+    [6-9][0-9]|100) ICON="󰕾"
     ;;
-    [3-5][0-9]) ICON="奔"
+    [3-5][0-9]) ICON="󰖀"
     ;;
-    [1-9]|[1-2][0-9]) ICON="奄"
+    [1-9]|[1-2][0-9]) ICON="󰕿"
     ;;
-    *) ICON="婢"
+    *) ICON="󰖁"
   esac
 
   sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
