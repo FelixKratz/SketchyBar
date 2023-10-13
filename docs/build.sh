dfd1a7db3e6daed6df47e686521425923ff5bc03
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-10-14 00:06:51 +0200

    formatting and slight modifications
    fixup

---
+++ docs/build.sh
@@ -0,0 +1,12 @@
+rm -rf build
+mkdir build
+scdoc < sketchybar-animate.5.scd > build/sketchybar-animate.5
+scdoc < sketchybar-components.5.scd > build/sketchybar-components.5
+scdoc < sketchybar-events.5.scd > build/sketchybar-events.5
+scdoc < sketchybar-items.5.scd > build/sketchybar-items.5
+scdoc < sketchybar-popup.5.scd > build/sketchybar-popup.5
+scdoc < sketchybar-query.5.scd > build/sketchybar-query.5
+scdoc < sketchybar-tips.5.scd > build/sketchybar-tips.5
+scdoc < sketchybar-types.5.scd > build/sketchybar-types.5
+scdoc < sketchybar.1.scd > build/sketchybar.1
+scdoc < sketchybar.5.scd > build/sketchybar.5
