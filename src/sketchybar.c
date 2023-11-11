04c7228870d8dc2f41fe48e59869dc3bfe69805a
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-11-11 22:53:53 +0100

    push version to 2.19.2

--- src/sketchybar.c
+++ src/sketchybar.c
@@ -26,7 +26,7 @@
 
 #define MAJOR 2
 #define MINOR 19
-#define PATCH 0
+#define PATCH 2
 
 #if __MAC_OS_X_VERSION_MAX_ALLOWED >= 140000
 extern CGError SLSWindowManagementBridgeSetDelegate(void* delegate);
