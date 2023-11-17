1fbf8559302f4e69e015f08a76e05ea84a93cef8
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-11-17 14:37:21 +0100

    push version to 2.19.4: collection of hotfixes

--- src/sketchybar.c
+++ src/sketchybar.c
@@ -26,7 +26,7 @@
 
 #define MAJOR 2
 #define MINOR 19
-#define PATCH 3
+#define PATCH 4
 
 #if __MAC_OS_X_VERSION_MAX_ALLOWED >= 140000
 extern CGError SLSWindowManagementBridgeSetDelegate(void* delegate);
