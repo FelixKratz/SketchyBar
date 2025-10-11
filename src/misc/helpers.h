bd9030fe1478c2546ef5791d4bc6ffbcb0eac3be
Eric Shen <52634785+erics118@users.noreply.github.com>
2025-10-11 11:07:02 -0400

    feat: add fn modifier (#764)

--- src/misc/helpers.h
+++ src/misc/helpers.h
@@ -189,6 +189,8 @@ static inline char* get_modifier_description(uint32_t modifier) {
         strcat(description, "alt,");
     if (modifier & kCGEventFlagMaskCommand)
         strcat(description, "cmd,");
+    if (modifier & kCGEventFlagMaskSecondaryFn)
+        strcat(description, "fn,");
 
     int len = strlen(description);
     if (len > 0 && description[len - 1] == ',') {
